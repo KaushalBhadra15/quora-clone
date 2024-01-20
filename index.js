1d8093f27e525088879a565fd10499330b9df2d3
KaushalBhadra15 <123623382+KaushalBhadra15@users.noreply.github.com>
2024-01-20 14:51:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,68 @@
+ const express =require("express");
+ const app=express();
+ const port=8080;
+ const path=require("path");
+ const { v4: uuidv4 } = require('uuid');
+ const methodOverride=require("method-override");
+ app.use(methodOverride("_method"));
+ app.use(express.urlencoded({extended:true}));
+ app.set("view engine","ejs");
+ app.set("views",path.join(__dirname,"view"));
+ app.use(express.static(path.join(__dirname,"public")));
+ let posts =[
+   {
+      id:uuidv4(),
+      username:"Kaushal",
+      content:"I love coding",
+   },
+   
+   {
+      id:uuidv4(),
+         username:"Sourav",
+         content:"Hello World!",
+   },
+   {
+      id:uuidv4(),
+      username:"Rohan",
+      content:"This is my first post",
+   },
+];
+ app.get("/posts",(req,res)=>{
+   res.render("index.ejs",{posts});   
+});
+app.get("/posts/new",(req,res)=>{
+   res.render("new.ejs");
+});
+app.post("/posts",(req,res)=>{
+   let {username,content}=req.body;
+   let id=uuidv4();
+   posts.push({id,username,content});
+   res.redirect("/posts")
+});
+app.get("/posts/:id",(req,res)=>{
+   let {id}=req.params;
+   let post=posts.find((p)=>id===p.id);
+   res.render("show.ejs",{post});
+});
+app.patch("/posts/:id",(req,res)=>{
+   let {id}=req.params;
+   let newContent=req.body.content;
+   let post=posts.find((p)=>id===p.id);
+   post.content=newContent;
+   console.log(post);
+   res.redirect("/posts");
+   res.send("Patch request working");
+});
+app.get("/posts/:id/edit",(req,res)=>{
+   let {id}=req.params;
+   let post=posts.find((p)=>id===p.id);
+   res.render("edit.ejs",{post});
+});
+app.delete("/posts/:id",(req,res)=>{
+   let {id}=req.params;
+   posts=posts.filter((p)=>id!==p.id);
+   res.redirect("/posts");
+});
+app.listen(port,()=>{
+    console.log("Listening to port :8080");
+});
